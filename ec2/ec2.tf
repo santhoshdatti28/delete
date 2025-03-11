72e366a205e35d4612ebf3213bc63cc13bb4556e
sample <sample@gmail.com>
2025-03-11 12:47:48 +0530

    ec2

---
+++ ec2/ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "this"{
+    ami = "ami-09c813fb71547fc4f"
+    vpc_security_group_ids= [aws_security_group.demo_sg.id]
+    instance_type= "t2.micro"
+    tags= {
+        Name= "test"
+    }
+}
+
+resource "aws_security_group" "demo_sg"{
+    name = "all"
+    description= "allow both inbound and outbound traffic"
+    ingress{
+        from_port = 22
+        to_port= 22
+        protocol = "tcp"
+        cidr_blocks= ["0.0.0.0/0"]
+    }
+    egress{
+        from_port= 0
+        to_port = 0
+        protocol= "-1"
+        cidr_blocks= ["0.0.0.0/0"]
+    }
+    tags= {
+        Name= "allow-all"
+    }
+}
